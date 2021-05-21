1573e210d2f46fa7314fa7507184a042051d4dfd
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-21 22:38:25 +0530

    Add files via upload

---
+++ ch7 p.3.py
@@ -0,0 +1,5 @@
+length=float(input("Enter Length of Rectangle:"))
+breadth=float(input("Enter Breadth of Rectangle:"))
+print("The Length of rectangle are as follows:", length)
+print("The Breadth of Rectangle are as follows:",breadth)
+print("Area of rectangle are as follows:",length*breadth)
