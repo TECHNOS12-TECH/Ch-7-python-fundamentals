cf16c5879dfd95d3c654058de54e2056a298c4bf
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-21 22:31:19 +0530

    Add files via upload

---
+++ ch7 p.2.py
@@ -0,0 +1,5 @@
+a=int(input("Enter 1st number"))
+b=int(input("Enter 2nd number"))
+c=int(input("Enter 3rd number"))
+print("The three number are as follows:",a, b, c)
+print("The sum of numbers are as follows:", a+b+c)
