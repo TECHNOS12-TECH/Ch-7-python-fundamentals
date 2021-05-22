480f626a820b17c55e519c63b6e24c242c776dc1
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-22 09:39:27 +0530

    Add files via upload

---
+++ ch7 p.9.py
@@ -0,0 +1,5 @@
+num1=int(input("Enter first number:\n"))
+num2=int(input("Enter second number:\n"))
+print("Orginal numbers(Without swapping):",num1,num2)
+num1,num2=num2,num1
+print("Numbers after swapping:",num1,num2)
