447ea4ea4b6438d967f5e80e53f9e35da8198d22
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-22 16:45:02 +0530

    Add files via upload

---
+++ ch7 type c 15.py
@@ -0,0 +1,7 @@
+a=int(input("Enter your 1st number:"))
+b=int(input("Enter your 2nd number:"))
+c=int(input("Enter your 3rd number:"))
+print("Orginal series of numbers:",a,b,c)
+a=a+b
+b=b+c
+print("After swapping:",a,b,c)
