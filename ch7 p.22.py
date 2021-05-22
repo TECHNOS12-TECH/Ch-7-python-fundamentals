2b3bd9af0e92145afb9271f9cb09178b51c37345
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-22 10:03:05 +0530

    Add files via upload

---
+++ ch7 p.22.py
@@ -0,0 +1,7 @@
+a=float(input("ENTER VALUE OF FIRST NUMBER:\n"))
+b=float(input("ENTER VALUE OF SECOND NUMBER:\n"))
+c=float(input("ENTER VALUE OF THIRD NUMBER:\n"))
+print("Orginal series of numbers:",a,b,c)
+b=a+b
+c=b+c
+print("After swapping the series of numbers (According to condition)",a,b,c)
