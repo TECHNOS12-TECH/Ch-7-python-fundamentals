1a31185e5a7460d341f122338db4463115669350
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-22 16:16:11 +0530

    Add files via upload

---
+++ ch7 type c 13.py
@@ -0,0 +1,12 @@
+n=input("Enter Student's name:")
+c=int(input("Enter Student's class:"))
+a=int(input("Enter Student's age:"))
+
+print("Student's deatils are as follows:","Name:",n,"Class:",c,"Age:",a)
+print()
+print()
+
+print("Student's deatils are as follows:")
+print("Name:",n)
+print("Class:",c)
+print("Age:",a)
